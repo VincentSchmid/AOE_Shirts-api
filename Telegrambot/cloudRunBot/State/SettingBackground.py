15dca57df3f3ebb280d240a9e7b8cdf72d205861
Vincent Schmid <Vincent.Schmid@ti8m.ch>
2021-12-12 10:58:38 +0100

    fixes setting background

--- Telegrambot/cloudRunBot/State/SettingBackground.py
+++ Telegrambot/cloudRunBot/State/SettingBackground.py
@@ -3,7 +3,7 @@ from .State import State
 
 class SettingBackground(State):
     def document_received(self, document):
-        self.background = document
+        self.model.background = document
         self.model.events.background_set()
 
     def stateInit(self):
