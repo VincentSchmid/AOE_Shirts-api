4f26e6517f96a8a5d7547d3e522098ecb9cfce89
Vincent Schmid <Vincent.Schmid@ti8m.ch>
2021-12-12 12:14:13 +0100

    updated help

--- Telegrambot/cloudRunBot/State/Help.py
+++ Telegrambot/cloudRunBot/State/Help.py
@@ -7,7 +7,7 @@ class Help(State):
 
         self.send_message("When you send /start the process will be begin")
         self.send_message("you can always send /start to reset the process to the beginning")
-        self.send_message("First, you send the background of your post (AOE screenshot size: 1080x1080)")
+        self.send_message("First, you send the background of your post (square crop of AOE screenshot)")
         self.send_message("Next, you will send the unedited photos of your shirt. Send as many pictures of your shirt as you want")
         self.send_message("Once you've sent your shirt images, confirm by sending the command /done")
         self.send_message("From now on, the bot will take over and process your images. It will crop out the shirts and place them on your background image and send the result as files")
